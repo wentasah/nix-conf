e5533e18108306f5f541f02ee01ff57dece2d66a
Michal Sojka <michal.sojka@cvut.cz>
2020-12-19 17:33:33 +0100

    sp: Configure ritchie as remote builder

--- steelpick/configuration.nix
+++ steelpick/configuration.nix
@@ -290,6 +290,18 @@ in
 #     "nixos-config=/etc/nixos/configuration.nix"
 #     "/nix/var/nix/profiles/per-user/root/channels"
   #   ];
+  nix.buildMachines = [ {
+    hostName = "ritchie";
+    system = "x86_64-linux";
+    # if the builder supports building for multiple architectures,
+    # replace the previous line by, e.g.,
+    # systems = ["x86_64-linux" "aarch64-linux"];
+    maxJobs = 1;
+    speedFactor = 2;
+    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+    mandatoryFeatures = [ ];
+  } ];
+  nix.distributedBuilds = true;
   nix.extraOptions = ''
     builders-use-substitutes = true
   '';
