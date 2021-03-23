5eeb62a327d1e70eba1a6c20e70a2155ce54268b
Michal Sojka <michal.sojka@cvut.cz>
2021-03-23 19:41:31 +0100

    Use tex-live scheme-full
    I've enough of searching for missing packages.

--- steelpick/home.nix
+++ steelpick/home.nix
@@ -8,9 +8,7 @@ let
 #     skypeforlinux = "${pkgs.skypeforlinux}/bin/skypeforlinux";
   };
   mytexlive = pkgs.texlive.combine {
-    inherit (pkgs.texlive) scheme-basic scheme-medium collection-langczechslovak
-      collection-xetex latexmk collection-latexextra
-      collection-mathscience chktex roboto cbfonts IEEEconf fontawesome5;
+    inherit (pkgs.texlive) scheme-full;
     pkgFilter = (pkg:
       pkg.tlType == "run"
       || pkg.tlType == "bin"
