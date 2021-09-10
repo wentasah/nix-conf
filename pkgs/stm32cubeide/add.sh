d5dd541c0d8f6a84cfe8bf80d5e046b53ae83fb3
Michal Sojka <michal.sojka@cvut.cz>
2021-09-10 12:40:34 +0200

    Try to make stm32cubeide gcroot work reliably

--- pkgs/stm32cubeide/add.sh
+++ pkgs/stm32cubeide/add.sh
@@ -3,4 +3,4 @@
 set -x
 
 nix_store_path="$(nix-store --add en.st-stm32cubeide_1.5.1_9029_20201210_1234_amd64.sh.zip)"
-nix-store -r "$nix_store_path" --add-root ./root-name --indirect
+nix-store --add-root ./nix-gcroot --indirect --realise "$nix_store_path"
