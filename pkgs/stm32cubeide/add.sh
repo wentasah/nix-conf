acee1b98080776f0d820d72abd52b70cad85b3b3
Michal Sojka <michal.sojka@cvut.cz>
2021-09-12 11:55:24 +0200

    Allow calling stm32cubeide/add.sh from arbitrary directory

--- pkgs/stm32cubeide/add.sh
+++ pkgs/stm32cubeide/add.sh
@@ -2,5 +2,7 @@
 
 set -x
 
+cd "$(dirname "$0")" || exit 1
+
 nix_store_path="$(nix-store --add en.st-stm32cubeide_1.5.1_9029_20201210_1234_amd64.sh.zip)"
 nix-store --add-root ./nix-gcroot --indirect --realise "$nix_store_path"
