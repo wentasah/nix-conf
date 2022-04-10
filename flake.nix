50073477655c5b2a21c63a5bba8c8517326a504d
Michal Sojka <michal.sojka@cvut.cz>
2022-04-10 18:01:31 +0200

    Workaround Firefox problem

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:wentasah/nixpkgs/master";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-21.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     home-manager = { url = "github:wentasah/home-manager"; inputs.nixpkgs.follows  = "nixpkgs"; };
     sterm = { url = "github:wentasah/sterm"; inputs.nixpkgs.follows  = "nixpkgs"; };
@@ -12,7 +13,7 @@
     emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, home-manager, sterm, notify-while-running, nix-ld, emacs-overlay }: {
+  outputs = { self, nixpkgs, nixpkgs-stable, nixos-hardware, home-manager, sterm, notify-while-running, nix-ld, emacs-overlay }: {
 
     nixosConfigurations.steelpick = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -33,7 +34,10 @@
 
             nixpkgs.overlays = [
               sterm.overlay
-              ( final: prev: { notify-while-running = import notify-while-running { pkgs = final; }; } )
+              ( final: prev: {
+                notify-while-running = import notify-while-running { pkgs = final; };
+                inherit (nixpkgs-stable.legacyPackages.x86_64-linux) firefox; # workaround https://github.com/NixOS/nixpkgs/issues/167785
+              } )
               emacs-overlay.overlay
             ];
           }
