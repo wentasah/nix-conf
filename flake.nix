5a54bee191067557d775a35354c67abe54bcd316
Michal Sojka <michal.sojka@cvut.cz>
2022-06-17 10:53:48 +0200

    Enable home-manager.useUserPackages
    This resolves the following error that happend during activation of
    updated configuration (excerpt from journalctl):
    
        hm-activate-xxx: Activating installPackages
        hm-activate-xxx: error: files '/nix/store/x7ivxlxyk24x7kvbvirc69rqpqiq58k3-home-manager-path/bin/bitmap2component' and '/nix/store/gyvj9h07x3zckkqb0k51avsvac6lcygl-home-manager-path/bin/bitmap2component' have the same priority 5; use 'nix>
        hm-activate-xxx: Oops, Nix failed to install your new Home Manager profile!
        hm-activate-xxx: Perhaps there is a conflict with a package that was installed using
        hm-activate-xxx: "nix profile install"? Try running
        hm-activate-xxx:	 nix profile list
        hm-activate-xxx: and if there is a conflicting package you can remove it with
        hm-activate-xxx:	 nix profile remove {number | store path}
        hm-activate-xxx: Then try activating your Home Manager configuration again.

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
-            # home-manager.useUserPackages = true;
+            home-manager.useUserPackages = true;
             home-manager.users.wsh = import ./steelpick/home.nix;
           }
           {
