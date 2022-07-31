6609d06c1f283fa83e51eec6aae97930433da85e
Michal Sojka <michal.sojka@cvut.cz>
2022-07-31 09:54:53 +0200

    julia-wrapper: Extra libraries are no longer needed
    It seems that Julia now contains the needed libraries and the only
    thing necessary to use Plots.jl is to set NIX_LD variable. I don't
    know why it is so, but it works.

--- pkgs/julia-wrapper/default.nix
+++ pkgs/julia-wrapper/default.nix
@@ -2,17 +2,17 @@
 let
   # Libraries needed by binary artefacts
   libs = [
-    freetype
-    libpng
-    openssl
-    qt5.qtbase
-    stdenv.cc.cc.lib
-    zlib
+#     freetype
+#     libpng
+#     openssl
+#     qt5.qtbase
+#     stdenv.cc.cc.lib
+#     zlib
   ];
 in
 writeShellScriptBin "julia" ''
   # Set variables for nix-ld
-  export NIX_LD_LIBRARY_PATH=${lib.makeLibraryPath libs}
+  #export NIX_LD_LIBRARY_PATH=${lib.makeLibraryPath libs}
   export NIX_LD=${stdenv.cc.libc}/lib/ld-linux-x86-64.so.2
   ${julia}/bin/julia "$@"
 ''
