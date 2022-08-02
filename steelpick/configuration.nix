e840ce7c8a40c0fc763e7f23434baf4c38514307
Michal Sojka <michal.sojka@cvut.cz>
2022-08-02 22:20:55 +0200

    Configure supportedLocales explicitly

--- steelpick/configuration.nix
+++ steelpick/configuration.nix
@@ -98,6 +98,7 @@ in
 
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales =  [ "en_US.UTF-8/UTF-8" "en_GB.UTF-8/UTF-8" "cs_CZ.UTF-8/UTF-8" ];
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
