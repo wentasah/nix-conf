f5fd77821a2caaa208cf857524780a301a11064c
Michal Sojka <michal.sojka@cvut.cz>
2022-08-09 23:09:40 +0200

    Package fastddsgen for Nix

--- modules/fastdds.nix
+++ modules/fastdds.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }:
 let
   fastdds = pkgs.callPackage ../pkgs/fastdds { };
+  fastddsgen = pkgs.callPackage ../pkgs/fastdds/fastddsgen.nix { };
 in
 {
   environment.systemPackages = [
     fastdds fastdds.fastcdr fastdds.foonathan-memory
+    fastddsgen
   ];
 
   # Without this cmake cannot find fastcdr includes
