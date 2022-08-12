d376c599b10e41fe4a219edc49f284a419180ee3
Michal Sojka <michal.sojka@cvut.cz>
2022-08-12 22:56:31 +0200

    Use fastddsgen submitted to nixpkgs
    https://github.com/NixOS/nixpkgs/pull/186326

--- modules/fastdds.nix
+++ modules/fastdds.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 let
   fastdds = pkgs.callPackage ../pkgs/fastdds { };
-  fastddsgen = pkgs.callPackage ../pkgs/fastdds/fastddsgen.nix { };
+  #fastddsgen = pkgs.callPackage ../pkgs/fastdds/fastddsgen.nix { };
 in
 {
   environment.systemPackages = [
     fastdds fastdds.fastcdr fastdds.foonathan-memory
-    fastddsgen
+    pkgs.fastddsgen
   ];
 
   # Without this cmake cannot find fastcdr includes
