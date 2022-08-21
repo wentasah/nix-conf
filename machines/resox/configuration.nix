e4960723ef8eb2a2fdf823789e507f3b1556e7c4
Michal Sojka <michal.sojka@cvut.cz>
2022-08-21 21:42:29 +0200

    Attempt to fix locales

--- machines/resox/configuration.nix
+++ machines/resox/configuration.nix
@@ -49,6 +49,8 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "cs_CZ.UTF-8";
+  i18n.supportedLocales =  [ "en_US.UTF-8/UTF-8" "en_GB.UTF-8/UTF-8" "cs_CZ.UTF-8/UTF-8" ];
+
   # console = {
   #   font = "Lat2-Terminus16";
   #   keyMap = "us";
