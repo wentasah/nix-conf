93c23243fdf75e91c594eb3c49a142f1d3e6dc7b
Michal Sojka <michal.sojka@cvut.cz>
2022-12-01 17:24:42 +0100

    Try enabling nsncd

--- machines/steelpick/configuration.nix
+++ machines/steelpick/configuration.nix
@@ -213,6 +213,8 @@ in
 
   # List services that you want to enable:
 
+  services.nscd.enableNsncd = true; # https://nixos.org/manual/nixos/stable/release-notes.html#sec-release-22.11-highlights
+
   services.avahi = {
     publish = {
       enable = true;
