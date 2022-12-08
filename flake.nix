7c6967774abdd6ff7b6fdb2733b3378e091ba319
Michal Sojka <michal.sojka@cvut.cz>
2022-12-08 18:03:20 +0100

    Set default zsh prompt for ritchie
    Without this, no prompt is shown in non-interactive sessions, which
    makes Tramp fail.

--- flake.nix
+++ flake.nix
@@ -112,6 +112,7 @@
               if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
                 . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
               fi
+              PS1='%m%# '
             '';
           }
         ];
