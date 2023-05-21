63ed59039b28f8d964e44053cbde5122030b2603
Michal Sojka <michal.sojka@cvut.cz>
2023-05-21 08:51:32 +0200

    Fix build of cargo-prefetch

--- pkgs/cargo-prefetch/default.nix
+++ pkgs/cargo-prefetch/default.nix
@@ -1,4 +1,4 @@
-{rustPlatform, fetchFromGitHub, pkg-config, openssl_1_1}:
+{rustPlatform, fetchFromGitHub, pkg-config, openssl}:
 rustPlatform.buildRustPackage rec {
   pname = "cargo-prefetch";
   version = "0.1.0";
@@ -6,15 +6,15 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "ehuss";
     repo = pname;
-    rev = "08bbc066b608ae849a4ff8c40aa64fa3193f90b0"; # version;
-    sha256 = "04m8n42x4fhjsybpr5idylihdmfbkcwfr8mz2vb1kq5lqs88hplj";
+    rev = "pull/5/head"; # version;
+    sha256 = "sha256-c1GcVWvz0yM89L4yNUlqaIR8SkatGpoUbOMVqVx5eGI=";
   };
+
+  cargoHash = "sha256-Fdq3iGw2ZoEo4N8xk1NEUm3cmQQCnMC+bTCAtVTrTxk=";
 
   nativeBuildInputs = [ pkg-config ];
 
-  # Broken with openssl 3; needs 1.1. See
-  # https://github.com/sfackler/rust-openssl/issues/1663 for similar error.
-  buildInputs = [ openssl_1_1 ];
+  buildInputs = [ openssl ];
 
   cargoSha256 = "01p1fv66axfimaz2xaj5phwdjr3m7ixy9mk9vbgmybrdwgk43rfa";
 }
