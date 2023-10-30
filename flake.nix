f24620079f1c1f60665cfc039b7c97565c608f8a
Michal Sojka <michal.sojka@cvut.cz>
2023-10-30 09:24:26 +0100

    Allow using sv-lang on stable

--- flake.nix
+++ flake.nix
@@ -102,7 +102,7 @@
                 (final: prev: {
                   # Packages from unstable
                   inherit (nixpkgs.outputs.legacyPackages.x86_64-linux)
-                    d2 julia-stable-bin nurl ikiwiki nil eza git-backdate typstfmt;
+                    d2 julia-stable-bin nurl ikiwiki nil eza git-backdate typstfmt sv-lang;
                   #xdg-desktop-portal = import ./pkgs/xdg-desktop-portal-1.17.0.nix { inherit final prev; };
                 })
               ];
