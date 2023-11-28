4517200f886a0b00a52bfb8217b30e7119194de4
Michal Sojka <michal.sojka@cvut.cz>
2023-11-28 09:24:12 +0100

    flamenco: 3.2 -> 3.3

--- pkgs/flamenco/default.nix
+++ pkgs/flamenco/default.nix
@@ -2,10 +2,10 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "flamenco";
-  version = "3.2";
+  version = "3.3";
   src = fetchTarball {
     url = "https://flamenco.blender.org/downloads/flamenco-${finalAttrs.version}-linux-amd64.tar.gz";
-    sha256 = "sha256:16jk0l8qv7h9j90knmd6avrc2x0ww3rwbfpsxj9m8n44jbyi1a60";
+    sha256 = "sha256:0k94s6rsrwaspck8vz124rlbwiqaml65kn84ljlwvwwzn4vmgpx9";
   };
 
   installPhase = ''
