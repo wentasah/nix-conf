ca49d7fed8be6266c208a899cdfe08223f2456a8
Michal Sojka <michal.sojka@cvut.cz>
2024-02-02 20:30:46 +0100

    Update nix-ld Julia wrapper

--- pkgs/julia-wrapper/default.nix
+++ pkgs/julia-wrapper/default.nix
@@ -1,18 +1,19 @@
-{ lib, writeShellScriptBin, julia, zlib, openssl, qt5, freetype, libpng, stdenv }:
+{ lib, writeShellScriptBin, julia, zlib, openssl, qt5, freetype, libpng, stdenv, libiconvReal }:
 let
-  # Libraries needed by binary artefacts
+  # Libraries needed by binary artifacts
   libs = [
-#     freetype
-#     libpng
-#     openssl
-#     qt5.qtbase
-#     stdenv.cc.cc.lib
-#     zlib
+    freetype
+    libiconvReal
+    libpng
+    openssl
+    qt5.qtbase
+    stdenv.cc.cc.lib
+    zlib
   ];
 in
 writeShellScriptBin "julia" ''
   # Set variables for nix-ld
-  #export NIX_LD_LIBRARY_PATH=${lib.makeLibraryPath libs}
+  export NIX_LD_LIBRARY_PATH=${lib.makeLibraryPath libs}
   export NIX_LD=${stdenv.cc.libc}/lib/ld-linux-x86-64.so.2
   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH''${LD_LIBRARY_PATH:+:}/run/opengl-driver/lib"
   exec ${julia}/bin/julia "$@"
