9fddc78d4f8a88d692075fe4e80b3c5dc4dc5f09
Michal Sojka <michal.sojka@cvut.cz>
2024-03-10 09:50:42 +0100

    Reenable cachix
    It's already fixed, only needs to propagate to the channel.

--- modules/home-base.nix
+++ modules/home-base.nix
@@ -28,7 +28,7 @@ in
     black-macchiato
     btdu
     btop
-    # cachix disabled for stable failure https://github.com/NixOS/nixpkgs/pull/294212
+    cachix
     ccls
     cloc
     cmake-language-server
