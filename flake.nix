62cfde836f465a34c9a112774f4904a483bbd4c8
Michal Sojka <michal.sojka@cvut.cz>
2024-04-09 22:32:55 +0200

    Use mcap-cli on stable from unstable

--- flake.nix
+++ flake.nix
@@ -131,7 +131,7 @@
                 (final: prev: {
                   # Packages from unstable
                   inherit (nixpkgs.outputs.legacyPackages.x86_64-linux)
-                    d2 julia-stable-bin nurl ikiwiki nil eza git-backdate typstfmt sv-lang;
+                    d2 julia-stable-bin nurl ikiwiki nil eza git-backdate typstfmt sv-lang mcap-cli;
                   # Veridian needs Rust from unstable
                   veridian = nixpkgs.outputs.legacyPackages.x86_64-linux.callPackage ./pkgs/veridian { };
                   #xdg-desktop-portal = import ./pkgs/xdg-desktop-portal-1.17.0.nix { inherit final prev; };
