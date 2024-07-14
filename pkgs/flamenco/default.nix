5a519556fb505f712f2230070cf4a26adfaca2d2
Michal Sojka <michal.sojka@cvut.cz>
2024-07-14 17:58:29 +0200

    flamenco: 3.3 -> 3.5

--- pkgs/flamenco/default.nix
+++ pkgs/flamenco/default.nix
@@ -1,18 +1,20 @@
 { stdenv
+, fetchurl
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "flamenco";
-  version = "3.3";
-  src = fetchTarball {
+  version = "3.5";
+  src = fetchurl {
     url = "https://flamenco.blender.org/downloads/flamenco-${finalAttrs.version}-linux-amd64.tar.gz";
-    sha256 = "sha256:0k94s6rsrwaspck8vz124rlbwiqaml65kn84ljlwvwwzn4vmgpx9";
+    hash = "sha256-ePbmR/s1Euc/GYWulH1QtCi52YMOy3X8C/THqBRkb54=";
   };
 
   installPhase = ''
     runHook preInstall
 
-    mkdir $out $out/bin
-    cp -r $src/* $out
+    tar xf $src
+    mkdir -p $out/bin
+    mv flamenco-${finalAttrs.version}-linux-amd64/* $out
     (cd $out/bin && ln -s ../flamenco-manager ../flamenco-worker .)
 
     runHook postInstall
