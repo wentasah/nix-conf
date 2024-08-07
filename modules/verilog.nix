6800a7e9fa6becdd487a11062301ff0a208b1943
Michal Sojka <michal.sojka@cvut.cz>
2024-08-07 10:57:30 +0200

    Use svls from nixpkgs instead of my local packaging

--- modules/verilog.nix
+++ modules/verilog.nix
@@ -1,14 +1,13 @@
 { pkgs, ... }:
 let
   svlangserver = pkgs.callPackage ../pkgs/svlangserver.nix { };
-  svls = pkgs.callPackage ../pkgs/svls.nix { };
 in
 {
   home.packages = [
     pkgs.sv-lang
     pkgs.verible
     svlangserver
-    svls
+    pkgs.svls
     pkgs.veridian
   ];
 }
