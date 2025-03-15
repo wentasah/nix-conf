83ba933f577993781b08088e6c1bb5cc2893c39a
Michal Sojka <michal.sojka@cvut.cz>
2025-03-15 22:15:43 +0100

    Configure goimapnotify via home-manager

--- machines/steelpick/home.nix
+++ machines/steelpick/home.nix
@@ -115,7 +115,6 @@ in
     glibcInfo                   # Not visible in emacs :-(
     gnome-tweaks
     #gnome3.nautilus
-    goimapnotify
     hdf5
     (hiPrio nixfmt-rfc-style)   # override nixfmt
     hugo
--- modules/mail.nix
+++ modules/mail.nix
@@ -1,7 +1,56 @@
 { config, pkgs, ... }:
-{
+let
+  isync = pkgs.isync.override { withCyrusSaslXoauth2 = true; };
+  goimapnotify = pkgs.goimapnotify.overrideAttrs ({ patches ? [], ... }: {
+    patches = patches ++ [ ../patches/goimapnotify-stderr.patch ];
+  });
+
+in {
   home.packages = with pkgs; [
-    (isync.override { withCyrusSaslXoauth2 = true; })
+    goimapnotify
+    isync
     oauth2ms
   ];
+
+  services.imapnotify = {
+    enable = true;
+    package = goimapnotify;
+    path = with pkgs; [
+      afew
+      bash
+      coreutils
+      hostname
+      isync
+      jq
+      libnotify
+      notmuch
+      oauth2ms
+    ];
+  };
+  # Run mailsync manually at start until
+  # https://gitlab.com/shackra/goimapnotify/-/issues/48 is resolved.
+  systemd.user.services.imapnotify-cvut = {
+    Service.ExecStartPre = config.accounts.email.accounts.cvut.imapnotify.onNotify;
+    Install.WantedBy = [ ]; # Don't start by default
+  };
+
+  accounts.email.accounts.cvut = {
+    primary = true;
+    address = "michal.sojka@cvut.cz";
+    userName = "sojkam1@cvut.cz";
+    passwordCommand = "oauth2ms";
+    imap = {
+      host = "outlook.office365.com";
+      port = 143;
+      tls.useStartTls = true;
+    };
+    imapnotify = {
+      enable = true;
+      boxes = [ "INBOX" ];
+      onNotify = "${config.home.homeDirectory}/bin/mailsync";
+      extraConfig = {
+        xoAuth2 = true;
+      };
+    };
+  };
 }
