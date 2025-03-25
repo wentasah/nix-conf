04ea83838eef941db623fc35d029e19c3fc4ebce
Michal Sojka <michal.sojka@cvut.cz>
2025-03-25 10:58:00 +0100

    Don't use incomaptible yazi plugins in stable

--- modules/home-base.nix
+++ modules/home-base.nix
@@ -374,7 +374,8 @@ in
         {on = [ "c" "m" ]; run  = "plugin chmod"; desc = "Chmod on selected files"; }
       ];
     };
-		plugins = {
+		plugins = if lib.hasPrefix "24.11" config.nixosConfigurations.resox.config.system.nixosVersion then {
+    } else {
 			chmod = "${yazi-plugins}/chmod.yazi";
       git = "${yazi-plugins}/git.yazi";
 		};  };
