fe6b18b7ff29fcad81f053c914209ed5c02e68dd
Michal Sojka <michal.sojka@cvut.cz>
2025-05-09 14:27:06 +0200

    Make swap hash and path in nix-direnv layout cache

--- modules/home-base.nix
+++ modules/home-base.nix
@@ -369,8 +369,9 @@ in
     direnv_layout_dir() {
       echo "''${direnv_layout_dirs[$PWD]:=$(
         local hash="$(sha1sum - <<<"''${PWD}" | cut -c-7)"
-        local path="''${PWD//[^a-zA-Z0-9]/-}"
-        echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+        local pwd_=''${PWD#/}
+        local path="''${pwd_//[^a-zA-Z0-9]/-}"
+        echo "''${XDG_CACHE_HOME}/direnv/layouts/''${path}-''${hash}"
       )}"
     }
   '';
