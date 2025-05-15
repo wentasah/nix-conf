555c8ad93df2ab00184c77897d7f2fdea10dd859
Michal Sojka <michal.sojka@cvut.cz>
2025-05-15 07:48:16 +0000

    foxglove-studio: 2.26.0 -> 2.27.1

--- pkgs/foxglove-studio/default.nix
+++ pkgs/foxglove-studio/default.nix
@@ -9,11 +9,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "foxglove-studio";
-  version = "2.26.0";
+  version = "2.27.1";
 
   src = fetchurl {
     url = "https://get.foxglove.dev/desktop/v${version}/foxglove-studio-${version}-linux-amd64.deb";
-    hash = "sha256-sWHd0nA/q27E+ufub4PL5R7sVKR/xvgYm98fRxPry9k=";
+    hash = "sha256-4RG9Wb9ULInRDyZVvbr0Qa222nkUCkc70XWBdvtt64E=";
   };
 
   nativeBuildInputs = [ dpkg makeWrapper ];
