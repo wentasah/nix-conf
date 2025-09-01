bc3503621c1a9119135b81e7f674cb1a97152b68
Michal Sojka <michal.sojka@cvut.cz>
2025-09-01 16:02:13 +0200

    Switch qt5 -> qt6

--- modules/qtcreator.nix
+++ modules/qtcreator.nix
@@ -2,11 +2,6 @@
 {
   home.packages = with pkgs; [
     qtcreator
-  ] ++  (with qt5; # To make qtcreator find the qt automatically
-    [ # This is qt5.full without insecure qtwebkit
-      qt3d qtcharts qtconnectivity qtdeclarative qtdoc qtgraphicaleffects qtimageformats
-      qtlocation qtmultimedia qtquickcontrols qtquickcontrols2 qtscript qtsensors qtserialport
-      qtsvg qttools qttranslations qtvirtualkeyboard qtwebchannel qtwebengine qtwebsockets
-      qtwebview qtx11extras qtxmlpatterns qtlottie
-    ]);
+    qt6.full
+  ];
 }
