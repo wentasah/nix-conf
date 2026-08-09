0ca8025c7cc323b7574b2967dd062bc507ef987b
Michal Sojka <michal.sojka@cvut.cz>
2026-08-09 23:51:43 +0200

    rpi: Switch to kernel from nixos-hardware

--- machines/rpi/configuration.nix
+++ machines/rpi/configuration.nix
@@ -2,12 +2,7 @@
 
 {
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
     initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-    };
   };
 
   fileSystems = {
