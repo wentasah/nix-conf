8a6411ad02e0ada6f5cd92fcf485532b59c9be84
Michal Sojka <michal.sojka@cvut.cz>
2026-09-24 22:41:51 +0200

    Add jai

--- machines/steelpick/configuration.nix
+++ machines/steelpick/configuration.nix
@@ -627,6 +627,7 @@ in
 #     };
   };
 
+  programs.jai-jail.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
